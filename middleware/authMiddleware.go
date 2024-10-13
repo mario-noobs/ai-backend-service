40f908ce6d6205d32f3420e703bdc803c56033ad
Bùi Ngọc Dũng <fcb2124@gmail.com>
2024-10-13 20:57:20 +0700

    [Update] CORS disabled

--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -32,3 +32,20 @@ func Authentication() gin.HandlerFunc {
 		c.Next()
 	}
 }
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "false")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
